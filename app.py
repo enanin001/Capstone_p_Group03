c6f669f6d90741bfebce40a41f7820c5302e4b3b
enanin001 <enanin@live.cn>
2024-02-22 21:26:06 -0500

    Feb22_Update

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/',methods=['POST', 'GET'])
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/workflow',methods=['POST', 'GET'])
+def workflow():
+    return render_template('workflow.html')
+
+@app.route('/display',methods=['POST', 'GET'])
+def display():
+    return render_template('display.html')
