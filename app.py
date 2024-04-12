30ad7bbae6fc178cb39599d0431c47ceee7d85da
enanin001 <enanin@live.cn>
2024-04-12 14:05:54 -0400

    Prediction and Result

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import cv2
+import os
 
 app = Flask(__name__)
+class_names = ['organic', 'recyclable']
+
+# Load the mbv2
+model = load_model('mobilenet_model')
+
+# Create the Folder for the 'uploads'
+UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def sharpen_and_adjust_brightness(img_array):
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    sharpened = cv2.filter2D(img_array, -1, kernel)
+    brightness_factor = np.random.uniform(0.8, 1.2)
+    adjusted = cv2.convertScaleAbs(sharpened, alpha=1, beta=brightness_factor * 25)
+    return adjusted
+
+def preprocess_image(image_path, target_size):
+    img = image.load_img(image_path, target_size=target_size)
+    img_array = image.img_to_array(img)
+    img_array = sharpen_and_adjust_brightness(img_array.astype('uint8'))
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return tf.keras.applications.mobilenet_v2.preprocess_input(img_array_expanded_dims)
+
+
 @app.route('/',methods=['POST', 'GET'])
 def home():
     return render_template('home.html')
@@ -12,6 +44,46 @@ def workflow():
 @app.route('/display',methods=['POST', 'GET'])
 def display():
     return render_template('display.html')
+
+# For the file type checking 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return "No file part", 400
+    
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename == '':
+             return "No file selected", 400
+        if file and allowed_file(file.filename):
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+            processed_image = preprocess_image(file_path, target_size=(160, 160))
+            prediction = model.predict(processed_image)
+            
+            prediction_probabilities = tf.nn.softmax(prediction).numpy()
+            #prediction = model.predict(processed_image).tolist()
+            
+            class_idx = np.argmax(prediction_probabilities)
+            class_name = class_names[class_idx]
+            confidence = np.max(prediction_probabilities)
+            os.remove(file_path)
+
+
+            """ return jsonify({
+            'class': class_name,
+            'confidence': float(confidence)
+            }) """
+            return render_template('prediction.html', class_name=class_name, confidence=float(confidence))
+            
+        else:
+            return jsonify(error="Incorrect File format "), 400
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
